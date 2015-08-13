366ce1f39f1cfbebcabd11539f7df2f38640b017
Niels Bertram <niels.bertram@fleurida.com>
2015-08-13 14:31:49 +1000

    added properly configured maven pom, whatever leiningen produces is unusable

--- project.clj
+++ project.clj
@@ -2,18 +2,23 @@
   :description "A MongoDB-backed store for Quartz Scheduler and Quartzite"
   :min-lein-version "2.4.2"
   :license {:name "Apache License 2.0"}
-  :dependencies [[org.quartz-scheduler/quartz   "2.1.7"]
+  :dependencies [[org.slf4j/slf4j-api           "1.7.10"]
+                 [org.quartz-scheduler/quartz   "2.1.7"]
                  [org.mongodb/mongo-java-driver "3.0.2"]
                  [joda-time/joda-time           "2.7"]
-                 [commons-codec/commons-codec   "1.10"]]
-  :java-source-paths ["src/main/java"]
-  :test-paths        ["src/test/clojure"]
+                 [commons-codec/commons-codec   "1.10"]
+                 [org.clojure/clojure           "1.7.0"]]
+  :plugins [[lein2-eclipse "2.0.0"]]
+  :source-paths ["src" "src/main/clojure"]
+  :java-source-paths ["src/main/java"]  ; Java source is stored separately.
+  :test-paths ["test" "src/test/clojure"]
+  :resource-paths ["src/main/resource"]
   :test-selectors {:all     (constantly true)
                    :focus   :focus
                    :default (constantly true)}
-  :javac-options     ["-target" "1.6" "-source" "1.6" "-Xlint:deprecation"]
+  :javac-options     ["-target" "1.7" "-source" "1.7"]
   :profiles {:dev {:resource-paths ["src/test/resources"]
-                   :dependencies [[org.clojure/clojure       "1.6.0"]
+                   :dependencies [[org.clojure/clojure       "1.7.0"]
                                   [clojurewerkz/quartzite    "1.3.0"]
                                   [com.novemberain/monger    "1.7.0"]
                                   [org.clojure/tools.logging "0.3.1" :exclusions [org.clojure/clojure]]
